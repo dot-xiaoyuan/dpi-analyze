4659588d764a480f88e279c721f53c59f076b12b
原桐 <yuantong@srun.com>
2024-09-19 17:05:57 +0800

    添加IP活动记录

---
+++ internal/analyze/activity/history.go
@@ -0,0 +1,9 @@
+package activity
+
+import (
+	"sync"
+)
+
+// IP 活动记录
+
+var IPActivityLogs sync.Map
--- internal/analyze/models.go
+++ internal/analyze/models.go
@@ -113,29 +113,3 @@ type FlowStatistics struct {
 	TotalPackets string `bson:"total_packets"`
 	Timestamp    string `bson:"timestamp"`
 }
-
-type TTLHistory struct {
-	TTL       uint8     `bson:"ttl"`
-	Timestamp time.Time `bson:"timestamp"`
-}
-
-type UAHistory struct {
-	UserAgent string    `bson:"user_agent"`
-	Timestamp time.Time `bson:"timestamp"`
-}
-
-type MacHistory struct {
-	MacAddress string    `bson:"mac_address"`
-	Timestamp  time.Time `bson:"timestamp"`
-}
-
-type IPActivityLogs struct {
-	IP               string       `bson:"ip"`
-	CurrentTTL       string       `bson:"current_ttl"`
-	TTLHistory       []TTLHistory `bson:"ttl_history"`
-	CurrentUserAgent string       `bson:"current_user_agent"`
-	UAHistory        []UAHistory  `bson:"ua_history"`
-	CurrentMac       string       `bson:"current_mac"`
-	MacHistory       []MacHistory `bson:"mac_history"`
-	LastSeen         time.Time    `bson:"last_seen"`
-}
