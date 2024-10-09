08e0b28c349f1fd07bdbdb23163d3ca8e16bbb7c
原桐 <yuantong@srun.com>
2024-10-09 16:51:24 +0800

    流日志响应数据结构调整

--- internal/web/controllers/stream.go
+++ internal/web/controllers/stream.go
@@ -2,28 +2,23 @@ package controllers
 
 import (
 	"context"
+	"github.com/dot-xiaoyuan/dpi-analyze/internal/web/common"
 	"github.com/dot-xiaoyuan/dpi-analyze/pkg/capture/types"
 	mongodb "github.com/dot-xiaoyuan/dpi-analyze/pkg/db/mongo"
+	"github.com/dot-xiaoyuan/dpi-analyze/pkg/utils"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 	"net/http"
-	"strconv"
 	"time"
 )
 
-type results struct {
-	Logs       []types.Sessions `json:"logs"`
-	TotalCount int64            `json:"totalCount"`
-	Err        error            `json:"err"`
-}
-
 func StreamList() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		pagination := utils.NewPagination(c.Query("page"), c.Query("page_size"))
+
 		collection := c.DefaultQuery("collection", time.Now().Format("stream-06-01-02-15"))
-		page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
-		pageSize, _ := strconv.Atoi(c.DefaultQuery("pageSize", "20"))
 		sortField := c.DefaultQuery("sortField", "_id")
 		sortOrder := c.DefaultQuery("sortOrder", "descend")
 		var orderBy int
@@ -33,12 +28,12 @@ func StreamList() gin.HandlerFunc {
 			orderBy = 1
 		}
 		// skip
-		skip := (page - 1) * pageSize
+		skip := (pagination.Page - 1) * pagination.Limit
 		zap.L().Info("query",
 			zap.String("collection", collection),
-			zap.Int("page", page),
-			zap.Int("pageSize", pageSize),
-			zap.Int("skip", skip),
+			zap.Int64("page", pagination.Page),
+			zap.Int64("limit", pagination.Limit),
+			zap.Int64("skip", skip),
 			zap.String("sortField", sortField),
 			zap.String("sortOrder", sortOrder),
 		)
@@ -58,7 +53,7 @@ func StreamList() gin.HandlerFunc {
 			{"$sort", bson.D{{sortField, orderBy}}},
 		}
 		limitStage := bson.D{
-			{"$limit", pageSize},
+			{"$limit", pagination.Limit},
 		}
 		skipStage := bson.D{
 			{"$skip", skip},
@@ -68,27 +63,25 @@ func StreamList() gin.HandlerFunc {
 		coll := mongodb.GetMongoClient().Database("dpi").Collection(collection)
 		cursor, err := coll.Aggregate(context.Background(), pipeline)
 
-		var result results
+		var result []types.Sessions
 		if err != nil {
 			zap.L().Error("mongodb.Aggregate", zap.Error(err))
-			result.Err = err
-			c.JSON(http.StatusInternalServerError, result)
+			common.ErrorResponse(c, http.StatusBadRequest, err.Error())
 		}
 		defer cursor.Close(context.Background())
 
 		for cursor.Next(context.Background()) {
 			var log types.Sessions
 			cursor.Decode(&log)
-			result.Logs = append(result.Logs, log)
+			result = append(result, log)
 		}
 
-		result.TotalCount, err = coll.CountDocuments(context.Background(), bson.D{})
+		pagination.Result = result
+		pagination.TotalCount, err = coll.CountDocuments(context.Background(), bson.D{})
 		if err != nil {
 			zap.L().Error("mongodb.cursor", zap.Error(err))
-			c.JSON(http.StatusInternalServerError, result)
+			common.ErrorResponse(c, http.StatusBadRequest, err.Error())
 		}
-		c.JSON(http.StatusOK, gin.H{
-			"results": result,
-		})
+		common.SuccessResponse(c, pagination)
 	}
 }
