795e48dd0261f1fbe7b2b2ed480764beffb612c3
原桐 <yuantong@srun.com>
2024-10-24 17:34:10 +0800

    调整json响应 - => _

--- pkg/component/types/ip.go
+++ pkg/component/types/ip.go
@@ -5,7 +5,7 @@ type Property string
 const (
 	TTL       Property = "ttl"
 	Mac       Property = "mac"
-	UserAgent Property = "user-agent"
+	UserAgent Property = "user_agent"
 )
 
 type Feature string
@@ -13,7 +13,7 @@ type Feature string
 const (
 	SNI         Feature = "sni"
 	HTTP        Feature = "http"
-	TLSVersion  Feature = "tls-version"
-	CipherSuite Feature = "cipher-suite"
+	TLSVersion  Feature = "tls_version"
+	CipherSuite Feature = "cipher_suite"
 	Session     Feature = "session"
 )
