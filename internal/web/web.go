ca6a98fec9551e2de831c3fa5a93d0016058f116
原桐 <yuantong@srun.com>
2024-11-01 11:11:06 +0800

    前端包

--- internal/web/web.go
+++ internal/web/web.go
@@ -82,21 +82,20 @@ func NewWebServer(c Config) {
 
 func ServerStatic(prefix string, embedFs embed.FS) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// 去掉前缀
 		fsys, err := fs.Sub(embedFs, prefix)
 		if err != nil {
 			panic(err)
 		}
 		fs2 := http.FS(fsys)
-		f, err := fs2.Open(ctx.Request.URL.Path)
+		requestedPath := ctx.Request.URL.Path
 
+		// 打开请求的路径文件
+		f, err := fs2.Open(requestedPath)
 		if err != nil {
-			// 文件不存在，尝试返回 index.html
+			// 如果文件不存在，返回 index.html
 			if errors.Is(err, fs.ErrNotExist) {
-				// 读取 index.html 文件
-				indexFile, err := fs2.Open("/index.html") // 注意：这里的路径可能需要根据你的项目结构调整
+				indexFile, err := fs2.Open("index.html")
 				if err != nil {
-					// 如果 index.html 也不存在，返回 404
 					ctx.String(http.StatusNotFound, "404 page not found")
 					return
 				}
@@ -105,11 +104,12 @@ func ServerStatic(prefix string, embedFs embed.FS) gin.HandlerFunc {
 				ctx.Abort()
 				return
 			}
-			// 如果是其他错误，继续调用下一个处理程序
+			// 其他错误
 			ctx.Next()
 			return
 		}
 
+		// 文件存在，直接提供服务
 		defer f.Close()
 		http.FileServer(fs2).ServeHTTP(ctx.Writer, ctx.Request)
 		ctx.Abort()
