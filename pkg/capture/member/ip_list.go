6a8b5551d843eeb89f7b6485fcab92c0cd32c136
原桐 <yuantong@srun.com>
2024-11-28 16:33:13 +0800

    终端列表优化，使用hscan加载进结构

--- pkg/capture/member/ip_list.go
+++ pkg/capture/member/ip_list.go
@@ -9,7 +9,6 @@ import (
 	v9 "github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
 	"strconv"
-	"time"
 )
 
 // IP 列表操作
@@ -20,13 +19,16 @@ type Tables struct {
 }
 
 type InfoJson struct {
-	IP       string `json:"ip"`
-	Username string `json:"username,omitempty"`
-	Mac      string `json:"mac,omitempty"`
-	TTL      string `json:"ttl,omitempty"`
-	UA       string `json:"user_agent,omitempty"`
-	Device   string `json:"device,omitempty"`
-	LastSeen string `json:"last_seen"`
+	IP       string  `json:"ip" redis:"ip"`
+	Username string  `json:"username,omitempty" redis:"username,omitempty"`
+	Mac      string  `json:"mac,omitempty" redis:"mac,omitempty"`
+	TTL      string  `json:"ttl,omitempty" redis:"ttl,omitempty"`
+	UA       string  `json:"user_agent,omitempty" redis:"user_agent,omitempty"`
+	Device   string  `json:"device,omitempty" redis:"device,omitempty"`
+	All      string  `json:"all,omitempty" redis:"all,omitempty"`
+	Mobile   string  `json:"mobile,omitempty" redis:"mobile,omitempty"`
+	Pc       string  `json:"pc,omitempty" redis:"pc,omitempty"`
+	LastSeen float64 `json:"last_seen" redis:"last_seen"`
 }
 
 // TraversalIP 遍历IP表
@@ -37,15 +39,21 @@ func TraversalIP(startTime, endTime int64, page, pageSize int64) (result Tables,
 	// 分页的起止索引
 	start := (page - 1) * pageSize
 
-	// Pipeline 批量查询
-	pipe := rdb.Pipeline()
 	var setName string
 	if config.FollowOnlyOnlineUsers {
 		setName = types.ZSetOnlineUsers
 	} else {
 		setName = types.ZSetIP
 	}
+	// 查询集合总数
 	result.TotalCount = rdb.ZCount(ctx, setName, strconv.FormatInt(startTime, 10), strconv.FormatInt(endTime, 10)).Val()
+	if result.TotalCount == 0 {
+		// 没数据直接返回
+		result.Results = nil
+		return
+	}
+	// Pipeline 批量查询
+	pipe := rdb.Pipeline()
 	// step1. 分页查询集合
 	zRangCmd := rdb.ZRevRangeByScoreWithScores(ctx, setName, &v9.ZRangeBy{
 		Min:    strconv.FormatInt(startTime, 10), // 查询范围的最小时间戳
@@ -61,13 +69,27 @@ func TraversalIP(startTime, endTime int64, page, pageSize int64) (result Tables,
 	}
 
 	ips := zRangCmd.Val()
+	if len(ips) == 0 {
+		result.Results = nil
+		result.TotalCount = 0
+		return
+	}
+
 	// step2. 批量获取每个IP详细信息
 	pipe = rdb.Pipeline()
-	ipCommands := make([]*v9.MapStringStringCmd, 0, len(ips))
+	ipCommands := make([]*v9.SliceCmd, 0, len(ips))
+	extraKeyCommands := make([]*v9.StringCmd, 0, len(ips)*3)
+
 	for _, ip := range ips {
 		key := fmt.Sprintf(types.HashAnalyzeIP, ip.Member.(string))
-		cmd := pipe.HGetAll(ctx, key)
-		ipCommands = append(ipCommands, cmd)
+		//cmd := pipe.HGetAll(ctx, key)
+		ipCommands = append(ipCommands, pipe.HMGet(ctx, key, "username", "mac", "ttl", "user_agent", "device"))
+
+		// 获取设备数量
+		allKey := pipe.Get(ctx, fmt.Sprintf(types.KeyDevicesAllIP, ip.Member.(string)))
+		mobileKey := pipe.Get(ctx, fmt.Sprintf(types.KeyDevicesMobileIP, ip.Member.(string)))
+		pcKey := pipe.Get(ctx, fmt.Sprintf(types.KeyDevicesPcIP, ip.Member.(string)))
+		extraKeyCommands = append(extraKeyCommands, allKey, mobileKey, pcKey)
 	}
 
 	_, err = pipe.Exec(ctx)
@@ -79,17 +101,34 @@ func TraversalIP(startTime, endTime int64, page, pageSize int64) (result Tables,
 	// step3. 处理查询结果
 	ipDetails := make([]InfoJson, 0, len(ips))
 	for i, cmd := range ipCommands {
-		info := cmd.Val()
-		detail := InfoJson{
-			IP:       ips[i].Member.(string),
-			Username: info["username"],
-			TTL:      info["ttl"],
-			UA:       info["user_agent"],
-			Mac:      info["mac"],
-			Device:   info["device"],
-			LastSeen: time.Unix(int64(ips[i].Score), 0).Format("2006/01/02 15:04:05"),
+		//info := cmd.Val()
+		var info InfoJson
+		err = cmd.Scan(&info)
+		if err != nil {
+			zap.L().Error("Scan", zap.Error(err))
+			continue
 		}
-		ipDetails = append(ipDetails, detail)
+
+		all := extraKeyCommands[i*3].Val()
+		mobile := extraKeyCommands[i*3+1].Val()
+		pc := extraKeyCommands[i*3+2].Val()
+
+		info.All, info.Mobile, info.Pc = all, mobile, pc
+		info.IP = ips[i].Member.(string)
+		info.LastSeen = ips[i].Score
+		//detail := InfoJson{
+		//	IP:       ips[i].Member.(string),
+		//	Username: info["username"],
+		//	TTL:      info["ttl"],
+		//	UA:       info["user_agent"],
+		//	Mac:      info["mac"],
+		//	Device:   info["device"],
+		//	All:      all,
+		//	Mobile:   mobile,
+		//	Pc:       pc,
+		//	LastSeen: ips[i].Score,
+		//}
+		ipDetails = append(ipDetails, info)
 	}
 
 	result.Results = ipDetails
