46d313133a653b75aa27faec176466ca8ddc1250
原桐 <yuantong@srun.com>
2024-12-24 17:01:18 +0800

    输出子网信息修改

--- cmd/nic.go
+++ cmd/nic.go
@@ -24,6 +24,7 @@ var NicCmd = &cobra.Command{
 			return
 		}
 
+		// 遍历所有网卡接口
 		for _, iface := range interfaces {
 			// 忽略没有启用或没有地址的接口
 			if iface.Flags&net.FlagUp == 0 || iface.Flags&net.FlagLoopback != 0 {
@@ -37,8 +38,10 @@ var NicCmd = &cobra.Command{
 				continue
 			}
 
-			// 只记录第一条有效地址
-			//var recorded bool
+			// 跟踪网卡的子网信息
+			var recorded bool
+
+			// 遍历该接口的所有地址
 			for _, addr := range addrs {
 				// 检查地址类型（IPv4 或 IPv6）
 				ipNet, ok := addr.(*net.IPNet)
@@ -46,31 +49,46 @@ var NicCmd = &cobra.Command{
 					continue
 				}
 
-				// 跳过链路本地地址（例如 fe80::）
-				//if ipNet.IP.IsLinkLocalUnicast() {
-				//	continue
-				//}
+				// 如果是 IPv6 地址，则跳过
+				if ipNet.IP.To4() == nil && len(addrs) == 1 {
+					continue
+				}
 
 				// 获取子网信息
 				ip, ipNet, err := utils.GetSubnetInfo(addr.String())
 				if err != nil {
 					fmt.Println("Error:", err)
-					return
+					continue
 				}
 				broadcast := utils.GetBroadcast(ipNet)
 				firstHost, lastHost := utils.GetIPRange(ipNet, broadcast)
-				table.Append([]string{
-					iface.Name,
-					ip.String(),
-					ipNet.Mask.String(),
-					ipNet.String(),
-					broadcast.String(),
-					fmt.Sprintf("%s - %s", firstHost, lastHost),
-				})
-				//recorded = true
-				continue // 只记录第一条
+
+				// 如果是第一次记录该网卡信息，则记录网卡名称
+				if !recorded {
+					table.Append([]string{
+						iface.Name, // 网卡名称
+						ip.String(),
+						ipNet.Mask.String(),
+						ipNet.String(),
+						broadcast.String(),
+						fmt.Sprintf("%s - %s", firstHost, lastHost),
+					})
+					recorded = true
+				} else {
+					// 如果网卡已经有记录，只需要填写子网相关信息
+					table.Append([]string{
+						"", // 合并单元格（网卡名称列空白）
+						ip.String(),
+						ipNet.Mask.String(),
+						ipNet.String(),
+						broadcast.String(),
+						fmt.Sprintf("%s - %s", firstHost, lastHost),
+					})
+				}
 			}
+
 		}
+
 		// 渲染表格
 		table.Render()
 	},
