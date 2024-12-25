a267cfc9cf01f8c86e9f59302d46063314f0ecf9
原桐 <yuantong@srun.com>
2024-12-25 16:23:18 +0800

    debug

--- internal/analyze/analyze.go
+++ internal/analyze/analyze.go
@@ -175,7 +175,7 @@ func (a *Analyze) HandlePacket(packet gopacket.Packet) {
 		}
 	}
 	// mDNS
-	if srcPort == "5353" || dstPort == "5353" && packet.NetworkLayer().LayerType() == layers.LayerTypeIPv4 && len(packet.ApplicationLayer().Payload()) > 0 {
+	if srcPort == "5353" || dstPort == "5353" && packet.NetworkLayer().LayerType() == layers.LayerTypeIPv4 && packet.ApplicationLayer() != nil {
 		payload := packet.ApplicationLayer().Payload()
 		device := protocols.ParseMDNS(payload, userIP, userMac)
 
