40b0ab568229bcccbd1f0ceab1e0eb4cda71af14
原桐 <yuantong@srun.com>
2024-12-26 16:45:27 +0800

    开启协议特征

--- pkg/capture/member/ip.go
+++ pkg/capture/member/ip.go
@@ -4,6 +4,7 @@ import (
 	"github.com/dot-xiaoyuan/dpi-analyze/pkg/capture/observer"
 	"github.com/dot-xiaoyuan/dpi-analyze/pkg/component/types"
 	"sync"
+	"time"
 )
 
 // IP 相关的核心逻辑
@@ -87,6 +88,6 @@ var handlers = map[types.Property]func(e PropertyChangeEvent){
 
 func Setup() {
 	go ChangeEventIP(Events)
-	//EnsureIndexOnce()
-	//go StartFlushScheduler(time.Minute)
+	EnsureIndexOnce()
+	go StartFlushScheduler(time.Minute)
 }
